dd3ec3a8fa1863a826f7891f6232feb2aa5cd647
Dejan Stankovic <ds0037@gmail.com>
2014-10-10 10:13:20 -0700

    Initial commit

---
+++ src/main/java/com/supplyframe/Main.java
@@ -0,0 +1,55 @@
+package com.supplyframe;
+
+import org.apache.poi.poifs.filesystem.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    if (args.length==0 || args.length>2) {
+      System.out.println("Please specify valid file");
+      return;
+    }
+
+    List<String> argsList = Arrays.asList(args);
+    if (!argsList.contains("-c")) {
+      unpackFile(new File(args[0]));
+    } else {
+      ParametricFile parametricFile = new ParametricFile();
+      POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(new File(args[args.length - 1])));
+      if (parametricFile.load(new DocumentInputStream((DocumentEntry) fs.getRoot().getEntry("FileHeader")))) {
+        System.out.println("Successful!");
+        System.out.println(parametricFile.getJSON().toString(2));
+      }
+    }
+  }
+  
+  private static void unpackFile(File file) throws IOException {
+    POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
+    exportDirEntry(fs.getRoot(), new File(file.getParentFile(), file.getName() + ".export"));
+  }
+
+  private static void exportDirEntry(DirectoryEntry dirEntry, File dir) throws IOException {
+    if (!dir.mkdirs()) throw new IOException("Can't create dir");
+
+    for (Entry entry : dirEntry) {
+      if (entry.isDirectoryEntry()) {
+        exportDirEntry((DirectoryEntry) entry, new File(dir, entry.getName()));
+      } else if (entry.isDocumentEntry()) {
+        exportFileEntry(entry, dir);
+      }
+    }
+  }
+
+  private static void exportFileEntry(Entry entry, File dir) throws IOException {
+    OutputStream output = new FileOutputStream(new File(dir, entry.getName()));
+    InputStream input = new DocumentInputStream((DocumentEntry) entry);
+
+    byte[] buffer = new byte[4096]; // Adjust if you want
+    int bytesRead;
+    while ((bytesRead = input.read(buffer)) != -1)
+      output.write(buffer, 0, bytesRead);
+  }
+
+}
---
+++ src/main/java/com/supplyframe/ParametricFile.java
@@ -0,0 +1,88 @@
+package com.supplyframe;
+
+import org.apache.poi.poifs.filesystem.DocumentInputStream;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ParametricFile {
+  private List<Map<String, Object>> records = new ArrayList<Map<String, Object>>();
+  public String signature;
+
+  public JSONObject getJSON() {
+    return new JSONObject(records.get(0));
+  }
+
+  public boolean load(DocumentInputStream inputStream) throws IOException {
+    records.clear();
+    while (inputStream.available() >= 4) {
+      Map<String, Object> record = readRecord(inputStream);
+      if (record != null && !record.isEmpty()) {
+        records.add(record);
+      }
+    }
+
+    signature = (String) records.remove(0).get("HEADER");
+
+    buildObjectHierarchy();
+
+    return !records.isEmpty();
+  }
+
+  private void buildObjectHierarchy() {
+    for (int i = records.size() - 1; i > 0; i--) {
+      Map<String, Object> current = records.get(i);
+      String s = (String) current.get("OWNERINDEX");
+      if (s == null || s.isEmpty())
+        s = "0";
+      int ownerIndex = Integer.decode(s);
+
+      records.remove(i);
+
+      Map<String, Object> owner = records.get(ownerIndex);
+      List<Object> children = (List<Object>) owner.get("children");
+      if (children == null) {
+        children = new ArrayList<Object>();
+        owner.put("children", children);
+      }
+
+      children.add(current);
+    }
+  }
+
+  private Map<String, Object> readRecord(DocumentInputStream inputStream) throws IOException {
+    String line = readLine(inputStream);
+
+    if (line == null) return null;
+
+    Map<String, Object> result = new HashMap<String, Object>();
+
+    String pairs[] = line.split("\\|");
+    for (String pair : pairs) {
+      if (pair.trim().isEmpty()) continue;
+
+      String[] data = pair.split("=");
+      if (data.length == 2) {
+        result.put(data[0], data[1]);
+      }
+    }
+
+    return result;
+  }
+
+  private String readLine(DocumentInputStream inputStream) throws IOException {
+    int length = inputStream.readInt();
+    if (length == -1) return null;
+
+    byte[] buffer = new byte[length];
+    inputStream.read(buffer, 0, length);
+    if (buffer[0] == 0) return null;
+
+    return new String(buffer).split("\u0000")[0];
+  }
+
+}
